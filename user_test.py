938289b6a67e5675073ec664214b1035a276548c
FiliTol <FiliTol@laptop.local>
2024-05-16 15:28:45 +0200

    Update

--- user_test.py
+++ user_test.py
@@ -1,10 +1,21 @@
 import os
+import sys
 from monstr.encrypt import Keys
 
-test = os.environ["VIRTUAL_ENV"]
 
-print(test)
-#PRIVKEY_NOSTR = os.environ['PRIVKEY_NOSTR']
-#k = Keys.get_key(PRIVKEY_NOSTR)
-#
-#print(PRIVKEY_NOSTR)
+def source_key(key: str = "nsec1al8wfs9g70etrcxzgxrfma9km9ae2n8jsl4rf7fjt6s3hvtrjawsnrjsv4") -> Keys:
+
+    if key:
+        if len(key) > 1:
+            k = Keys(priv_k=key)
+        else:
+            the_key: str = key
+            if the_key.startswith("nsec"):
+                k = Keys(priv_k=the_key)
+            else:
+                k = Keys(pub_k=the_key)
+
+    else:
+        k = Keys()
+
+    return k
